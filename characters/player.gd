ee937b07eee28238cbd5f71939e5bdf7bc52758d
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-05-11 12:57:02 +0100

    add dialogic

--- characters/player.gd
+++ characters/player.gd
@@ -10,8 +10,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("up"):
+		Dialogic.start("res://dialog/timeline/timeline1.dtl")
 	var playerSprite:AnimatedSprite2D =$playerSprite
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
